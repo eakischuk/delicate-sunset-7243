1fba6e61f4e8f2ee065dddcf6434f3cf4d5f1b46
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-24 09:35:26 -0600

    Add instance method adult passengers test

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Airline do
+  before(:each) do
+    @fronteir = Airline.create!(name: 'Fronteir')
+    @southwest = Airline.create!(name: 'Southwest')
+
+    @steve = Passenger.create!(name: 'Steve', age: 52)
+    @meg = Passenger.create!(name: 'Meg', age: 34)
+    @albert = Passenger.create!(name: 'Albert', age: 79)
+    @ally = Passenger.create!(name: 'Ally', age: 13)
+
+    @james = Passenger.create!(name: "James Potter", age: 43)
+
+    @flight_1 = Flight.create!(number: 1, date: '08/03/21', departure_city: 'Denver', arrival_city: 'Portland', airline: @fronteir)
+    @flight_2 = Flight.create!(number: 2, date: '08/15/21', departure_city: 'Indianapolis', arrival_city: 'San Francisco', airline: @fronteir)
+    @flight_3 = Flight.create!(number: 3, date: '08/01/21', departure_city: 'Indianapolis', arrival_city: 'Denver', airline: @southwest)
+
+    @flight_1.passengers << @steve
+    @flight_1.passengers << @ally
+    @flight_1.passengers << @meg
+
+    @flight_2.passengers << @albert
+    @flight_2.passengers << @steve
+
+    @flight_3.passengers << @james
+  end
+
   describe 'relationships' do
     it { should have_many(:flights) }
+    it { should have_many(:passengers).through(:flights) }
+  end
+
+  describe 'instance methods' do
+    it 'has distinct adult passengers' do
+      expect(@fronteir.adult_passengers).to eq([@steve, @meg, @albert])
+    end
   end
 end

c9860f2bd2ff0d85d37e9dd40359733bf74f05ed
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-24 09:08:30 -0600

    Add instance method flight_passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def flight_passenger(passenger_id)
+    flight_passengers.where('passenger_id = ?', passenger_id).first
+  end
 end
